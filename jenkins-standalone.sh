a6d9a27c225d7fd53048afd11161119514b4a3d8
mk-qi <root@mkrss.com>
2015-03-18 11:52:56 +0800

    modified:   jenkins-standalone.sh

--- jenkins-standalone.sh
+++ jenkins-standalone.sh
@@ -31,7 +31,7 @@ if [[ $? != 0 ]]; then
 fi
 
 # Accept ZooKeeper paths on the command line
-if [[ ! $# > 2 ]]; then
+if [[ ! $# > 1 ]]; then
     echo "Usage: $0 -z zk://10.132.188.212:2181[, ... ]/mesos"
     echo
     exit 1
